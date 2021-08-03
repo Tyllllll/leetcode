decce98f991a491776d5d2cabf33ce4b9cbb7d28
Tyllllll <WanghxTyl@outlook.com>
2021-08-03 13:20:36 +0800

    commit

--- 0473.js
+++ 0473.js
@@ -3,14 +3,33 @@
  * @return {boolean}
  */
 var makesquare = function (matchsticks) {
+  let res = false
   const sum = matchsticks.reduce((a, b) => a + b)
   if (sum % 4 !== 0) {
-    return
+    return res
   }
   const sideLenth = sum / 4
-  const dfs = (arr, ) => {
-    
+  const dfs = (culLength, i, alternative, success) => {
+    if (res) {
+      return
+    }
+    if (success === 4) {
+      res = true
+      return
+    }
+    if (i > matchsticks.length) {
+      return
+    }
+    if (culLength === sideLenth) {
+      dfs(0, 0, success + 1)
+    }
+    if (culLength + matchsticks[i] <= sideLenth) {
+      const [num] = matchsticks.splice(i, 1)
+      dfs(culLength + num, i, success)
+      dfs(culLength, i + 1, success)
+    }
   }
+  dfs(0, 0, 0)
+  return res
 }
-console.log(makesquare([1, 1, 2, 2, 2]))
-console.log(makesquare([3, 3, 3, 3, 3]))
+console.log(makesquare([5, 5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 3]))
