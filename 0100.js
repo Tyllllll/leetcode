6c88772b087c57e099b5a06b549b62705d578627
Tyllllll <874026556@qq.com>
2021-11-04 21:56:33 +0800

    0100

---
+++ 0100.js
@@ -0,0 +1,21 @@
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val
+  this.left = left === undefined ? null : left
+  this.right = right === undefined ? null : right
+}
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+  if (!p && !q) {
+    return true
+  } else if (!p || !q) {
+    return false
+  } else if (p.val === q.val) {
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+  } else {
+    return false
+  }
+}
