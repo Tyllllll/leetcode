7a733e015ed4abc0f5b887cb35f9a36a1f108dd3
Tyllllll <874026556@qq.com>
2021-12-11 21:35:18 +0800

    添加jest，改变文件路径

---
+++ src/剑指68_1.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+  const path1 = []
+  const path2 = []
+  const dfs = (node) => {
+    
+  }
+}
---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+test('用例1', () => {
+  expect(3).toBe(3)
+})
+
+test('用例2', () => {
+  expect(3).toBe(3)
+})
+
+test('用例3', () => {
+  expect(3).toBe(3)
+})
